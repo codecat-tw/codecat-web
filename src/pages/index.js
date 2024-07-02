69b7e9ba9651f0c0b7764b86ea6c3ddea3cad876
yd-tw <yd960528@gmail.com>
2024-07-02 23:52:33 +0800

    Update some text

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            熱門內容: 科技週刊！
           </Link>
         </div>
       </div>
