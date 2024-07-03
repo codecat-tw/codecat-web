ae529455971837b1e938bf29dee095e1f7e6f878
yd-tw <yd960528@gmail.com>
2024-07-03 12:49:07 +0800

    Change config more readable

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,22 +5,14 @@ const config = {
   title: '程式貓',
   tagline: '讓科技變得跟貓咪一樣可愛',
   favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
   url: 'https://codecat.tw',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'zh-hant',
-    locales: ['zh-hant'],
+    defaultLocale: 'zh-Hant',
+    locales: ['zh-Hant'],
   },
 
   presets: [
@@ -30,15 +22,9 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          //editUrl:'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          //editUrl:'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: './src/css/custom.css',
