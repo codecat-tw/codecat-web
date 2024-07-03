1ac4ff0ddd182381cd89d2748f942608d41e5bea
yd-tw <yd960528@gmail.com>
2024-07-03 18:18:00 +0800

    Fix broken link

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/technews/intro">
             熱門內容: 科技週刊！
           </Link>
         </div>
