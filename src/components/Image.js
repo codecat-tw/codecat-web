9357f966732f110cd4347f63d884e18e9d14c2ec
yd-tw <yd960528@gmail.com>
2024-07-05 23:19:55 +0800

    Add img comp can let img to center

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
 const Image = ({ path, alt }) => {
+  const containerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  };
+
+  const imageStyle = {
+    maxWidth: '100%',
+    height: 'auto',
+  };
+
   return (
-    <div>
-      <img src={require(`@site/static${path}`).default} alt={alt} />
+    <div style={containerStyle}>
+      <img src={require(`@site/static${path}`).default} alt={alt} style={imageStyle} />
     </div>
   );
 };
